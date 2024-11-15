5a1d709f7e8d4b537b215e0a7dd0787d20d83b60
EldodeGraaf <eldo.de.graaf@student.uva.nl>
2024-11-15 15:59:31 +0100

    Adding docstrings
    Also changing the code a bit to completely satisfy the exercise. Now ready to submit

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -4,35 +4,39 @@ from numpy import pi
 import pandas as pd
 
 @click.group()
-def smallangle():
+def small_group():
     pass
 
-@smallangle.command()
+@small_group.command()
 @click.option(
     "-n",
     "--number",
-    default=1,
-    help="Number of times to print.",
+    default=10,
+    help="Number of values of sin.",
     show_default=True, 
 )
 def sin(number):
+    """Calculates the sin of x-values, with x between 0 and 2pi.
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-@smallangle.command()
+@small_group.command()
 @click.option(
     "-n",
     "--number",
-    default=1,
-    help="Number of times to print.",
+    default=10,
+    help="Number of values of tan.",
     show_default=True, 
 )
 def tan(number):
+    """Calculates the tan of x-values, with x between 0 and 2pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
 
 if __name__ == "__main__":
-    smallangle()
+    small_group()
