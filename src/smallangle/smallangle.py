14e64044754e626d0eefaa8274424269be2b3491
EldodeGraaf <eldo.de.graaf@student.uva.nl>
2024-11-15 15:34:08 +0100

    Copied + eddited
    Copied smallangle and then made the changes, now works like expected

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,13 +3,31 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def smallangle():
+    pass
 
+@smallangle.command()
+@click.option(
+    "-n",
+    "--number",
+    default=1,
+    help="Number of times to print.",
+    show_default=True, 
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@smallangle.command()
+@click.option(
+    "-n",
+    "--number",
+    default=1,
+    help="Number of times to print.",
+    show_default=True, 
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -17,4 +35,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    smallangle()
